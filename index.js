60379e83e9ccd8b092e18ee4641667633db3de02
krivera1015 <krivera1015@gmail.com>
2018-06-26 17:45:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name)
   return kittens;
 }
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
